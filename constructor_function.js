c558939b105e94911a668637c19af770a6825b45
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-10-21 22:13:13 +0900

    Person ??

--- constructor_function.js
+++ constructor_function.js
@@ -1,9 +1,9 @@
 // 생성자 함수
 // 생성자 함수는 일반 함수와 비슷하게 선언한다.
-// 생성자 함수에서 this는 함수명을 가리킴
+// 생성자 함수에서 this는 생성된 인스턴스 가리킴
 
 function Person(name, age){
-    this.name = name;
+    Person.name = name;
     this.age = age;
 };
 
