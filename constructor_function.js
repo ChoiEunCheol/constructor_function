7054b1f7b0e49aa64e31a61b6061b1d27a961e74
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-10-22 22:57:21 +0900

    생성자 함수의 특징

--- constructor_function.js
+++ constructor_function.js
@@ -1,14 +1,17 @@
 // 생성자 함수
 // 생성자 함수는 일반 함수와 비슷하게 선언한다.
 // 생성자 함수에서 this는 생성된 인스턴스 가리킴
+// 생성자 함수는 인스턴스 생성 -> 초기화 -> 반환의 과정을 거침
+// return문은 생략
 
 function Person(name, age){
     this.name = name;
     Person.age = age;
+    console.log(this);
 
-    this.hi = ()=>{
-        console.log(this.name + " ㅎㅇ");
-    }
+    // this.hi = ()=>{
+    //     console.log(this.name + " ㅎㅇ");
+    // }
 };
 
 
@@ -16,4 +19,4 @@ function Person(name, age){
 let person1 = new Person("헨니",26);
 
 console.log(person1);
-person1.hi();
+// person1.hi();
